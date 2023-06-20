82416b524b8c306789e2e6acf7c52635c81aa06a
Jose Manzaneque Ramos <vaulner@gmail.com>
2023-06-20 16:59:54 +0200

    Update alpine-resizefs.sh

--- files/alpine-resizefs.sh
+++ files/alpine-resizefs.sh
@@ -2,10 +2,10 @@
 
 echo 1 > /sys/class/block/sda/device/rescan
 sleep 2
-GROWPART_OUT=`growpart /dev/sda 1`
+GROWPART_OUT=`growpart /dev/sda 3`
 if [ $? -eq 0 ]; then
     echo "trying to resize fs"
     echo $GROWPART_OUT
-    resize2fs /dev/sda1
+    resize2fs /dev/sda3
     echo "resize done"
 fi
