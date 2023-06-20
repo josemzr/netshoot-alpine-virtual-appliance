b53c330312fb028e9fb650ffc5fdaf3387cc0f17
Jose Manzaneque Ramos <vaulner@gmail.com>
2023-06-20 10:46:04 +0200

    Update alpine-netshoot.sh

--- scripts/alpine-netshoot.sh
+++ scripts/alpine-netshoot.sh
@@ -6,9 +6,9 @@
 set -xeu 
 echo '> Setup Netshoot Packages...'
 
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories \
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories \
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories \
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
 apk update 
 apk upgrade 
 apk add --no-cache \
