e97bbd42247f9bb87d334e9677c30405a67765d5
Jose Manzaneque Ramos <vaulner@gmail.com>
2023-06-20 09:50:08 +0200

    Update and rename debian-settings.sh to alpine-settings.sh

--- scripts/alpine-settings.sh
+++ scripts/alpine-settings.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eux
 
 ##
-## Debian Settings
+## Alpine Settings
 ## Misc configuration
 ##
 
-echo '> Debian Settings...'
+echo '> Alpine Settings...'
 
 echo '> SSH directory'
 mkdir -vp $HOME/.ssh
@@ -14,10 +14,10 @@ echo '> Adding vimrc'
 echo "set mouse-=a" >> /root/.vimrc
 
 echo '> Debian acts as a Router now'
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
 echo '> Disable IPv6'
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
 
 echo '> Setup Appliance Banner for /etc/issue & /etc/issue.net'
 echo "=============================" | tee /etc/issue /etc/issue.net > /dev/null
@@ -26,13 +26,6 @@ echo "=============================" | tee -a /etc/issue /etc/issue.net > /dev/n
 sed -i 's/#Banner none/Banner \/etc\/issue.net/g' /etc/ssh/sshd_config
 
 
-echo '> Setup Netshoot hostname and adding it to /etc/hosts'
-hostnamectl set-hostname netshoot
-sed -i s/debian/netshoot/g /etc/hosts
-
 sed -i 's/#Banner none/Banner \/etc\/issue.net/g' /etc/ssh/sshd_config
-
-echo '> Run debian-setup script at boot'
-echo "@reboot root  if [ ! -f /etc/ran_customization ]; then /opt/debian-setup.sh &> /var/log/debian-customization.log; fi" >> /etc/crontab
 
 echo '> Done'
